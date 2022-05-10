ce33b3c0f29648aaf1e4df5626377a115a93734d
Fernando Mercado <fermercado@live.com>
2022-05-10 15:39:00 -0300

    add information products

---
+++ projects.js
@@ -0,0 +1,49 @@
+let projectJson = [
+  {
+    tittle: 'F1-Fans',
+    img: 'img/f1-fans.png',
+    description: 'Pagina desenvolvida como conceito mobile first, usando as ferramentas HTML, CSS e JavaScript. Utilizando accordion list e animation scroll. Versão mobile mais interativa escondendo e mostrando conteudo conforme o click o usuario.',
+    deploy: 'https://fermercado.github.io/F1-fans/',
+    github: 'https://github.com/fermercado/F1-fans'
+  },
+  {
+    tittle: 'Loja LadoDicá',
+    img: 'img/lojaladodica.png',
+    description: 'E-commerce voltado para o vestuário infantil, pagina responsiva desenvolvida usando as ferramentas HTML, CSS e JavaScript. -Implementando Carrinho de compra (CRUD) / Cupom de desconto / Carrossel de fotos. Adicionando produtos de forma dinamica simulando um banco de dados',
+    deploy: 'https://fermercado.github.io/Loja-LadoDica/',
+    github: 'https://github.com/fermercado/Loja-LadoDica'
+  },
+  {
+    tittle: 'Site Intistucional',
+    img: 'img/siteinstitucional.png',
+    description: 'Página desenvolvida em conceito mobile first, tendo a finalidade de ser uma pagina instucional de uma empresa, utilizando HTML/CSS/JavaScript, com scroll suave e menu hamburguer na versão mobile.',
+    deploy: 'https://fermercado.github.io/Site-Institucional/',
+    github: 'https://github.com/fermercado/Site-Institucional'
+  },
+  {
+    tittle: 'Login e Cadastro',
+    img: 'img/logincadastro.png',
+    description: 'Projeto responsivo utilizando HTML/CSS/JavaScript, com validação por keyup e armazenando informações no localstorage.',
+    deploy: 'https://fermercado.github.io/Tela-de-login-e-cadastro/',
+    github: 'https://github.com/fermercado/Tela-de-login-e-cadastro'
+  },
+  {
+    tittle: 'ToDo-List',
+    img: 'img/todolist.png',
+    description: 'Pagina responsiva desenvolvida usando as ferramentas HTML, CSS e JavaScript, nele é possivel adicionar tarefas, podendo inserir descrições em casa uma delas individualmente, dar check nas tarefas, marcando elas como concluidas e alterando a cor, remover tarefas e salvar todos os dados e alterações em LOCALSTORAGE',
+    deploy: 'https://fermercado.github.io/Todo-list/',
+    github: 'https://github.com/fermercado/Todo-list'
+  },
+  {
+    tittle: 'Portifólio',
+    img: 'img/portifólio.png',
+    description: 'em construção',
+    deploy: 'https://fermercado.github.io/Portifolio/',
+    github: 'https://github.com/fermercado/Portifolio'
+  }
+
+
+
+
+
+]
